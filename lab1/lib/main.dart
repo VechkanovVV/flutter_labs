75bffa8d620c140408da4d931fd2edf1f577ae44
Spiliviler <hatavidit84@gmail.com>
2023-06-21 22:14:45 +0300

    second commit

--- lab1/lib/main.dart
+++ lab1/lib/main.dart
@@ -1,125 +1,193 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(Lab1());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// this is your APP Main screen configuration
+class Lab1 extends StatelessWidget {
+  Lab1({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LAb1HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+/// this is a template to start building a UI
+/// to start adding any UI you want change what comes after the [ body: ] tag below
+class LAb1HomePage extends StatelessWidget {
+  LAb1HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xfff4f4f4),
+      /*******************--[focus here 🧐]--*******************/
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        leading: const Icon(Icons.stars),
+        title: const Text('Lab 1'),
+        backgroundColor: Colors.teal,
+        elevation: 4,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: myWidget(),
+      /*******************--[focus here 🧐]--*******************/
+    );
+  }
+
+  Widget myWidget() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          /*******************--[focus here 🧐]--*******************/
+          SizedBox(
+            height: 25,
+          ),
+          exercise1(),
+          SizedBox(
+            height: 25,
+          ),
+          exercise2(),
+          SizedBox(
+            height: 25,
+          ),
+          exercise3(),
+          SizedBox(
+            height: 25,
+          ),
+          exercise4(),
+          SizedBox(
+            height: 25,
+          ),
+          exercise5(),
+          SizedBox(
+            height: 25,
+          ),
+          exercise6(),
+          /*******************--[focus here 🧐]--*******************/
+        ],
+      ),
+    );
+  }
+
+  /// TODO: Implement Exercises below as per the handed Document
+  Widget exercise1() {
+    return Text(
+      "Welcom to lab1",
+      style: TextStyle(
+        fontFamily: 'monospace',
+        color: Colors.lightGreenAccent,
+        backgroundColor: Colors.grey,
+        fontSize: 40,
+      ),
+    );
+  }
+
+  Widget exercise2() {
+    return Icon(
+      Icons.share,
+      color: Colors.blue,
+      size: 90,
+    );
+  }
+
+  /// TODO : print on the screen on Pressed when clicking on the button, and print on Long Pressed when long click to Button
+  Widget exercise3() {
+    return Container(
+      width: 250,
+      height: 50,
+      child: ElevatedButton(
+        onLongPress: () {
+          print('long click');
+        },
+        onPressed: () {
+          print('short click');
+        },
+        child: Text(
+          'Click here',
+          style: TextStyle(color: Colors.yellow, fontSize: 20),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blueAccent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget exercise4() {
+    return Container(
+      width: 60,
+      height: 60,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        onPressed: () {},
+        child: Icon(
+          Icons.favorite,
+          color: Colors.greenAccent,
+        ),
+      ),
+    );
+  }
+
+  Widget exercise5() {
+    return Container(
+      width: 200,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.yellow,
+          width: 4,
+        ),
+      ),
+      child: TextButton(
+        onPressed: () {},
+        child: Text(
+          'Button to press',
+          style: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget exercise6() {
+    return Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          border: Border(
+            top: BorderSide(
+              color: Colors.blue,
+              width: 8,
+            ),
+            left: BorderSide(
+              color: Colors.indigoAccent,
+              width: 8,
+            ),
+            bottom: BorderSide(
+              color: Colors.red,
+              width: 8,
+            ),
+            right: BorderSide(
+              color: Colors.purple,
+              width: 8,
+            ),
+          ),
+        ),
+        child: Center(
+          child: Icon(
+            Icons.warning,
+            size: 40,
+          ),
+        ));
   }
 }
